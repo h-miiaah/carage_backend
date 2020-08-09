39801e387071d5e3979244af6d7a85ad52b0ae6c
h-miiaah <haim.miiaah5@gmail.com>
2020-08-09 19:57:02 -0400

    added image url to cars table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_230203) do
+ActiveRecord::Schema.define(version: 2020_08_09_235545) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_08_09_230203) do
     t.integer "brand_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_url"
   end
 
 end
