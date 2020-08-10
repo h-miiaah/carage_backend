3d433f4a459ac58266c699bc3227e8759d1a70d2
h-miiaah <haim.miiaah5@gmail.com>
2020-08-09 20:58:23 -0400

    added index and create routes to cars controller and added routes in routes rb file

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::CarsController < ApplicationController
+
+    def index
+        cars = Car.all
+        render json: cars
+    end
+
+    def create
+        car = Car.new(car_params)
+        if car.save
+            render json: car, status: accepted
+        else
+            render json: {errors: car.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def car_params
+        params.require(:car).permit(:name, :year, :color, :mileage, :image_url, :brand_id)
+    end
+
 end
